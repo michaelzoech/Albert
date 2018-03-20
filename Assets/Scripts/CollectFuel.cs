cd70cdabc9713aa552d54d64e9097ecf38931d9c
Michael Zoech <michi.zoech@gmail.com>
2018-03-20 16:32:50 -0700

    Collect fuel and show as score

---
+++ Assets/Scripts/CollectFuel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectFuel : MonoBehaviour {
+
+	private GameController gameController;
+
+	void Start() {
+		GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+		gameController = gameControllerObject.GetComponent<GameController>();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Destroy(gameObject);
+		gameController.OnFuelCollected();
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public Text ScoreText;
+
+	private int fuelCollected;
+
+	void Start () {
+		fuelCollected = 0;
+	}
+
+	public void OnFuelCollected() {
+		fuelCollected++;
+		ScoreText.text = "Score: " + fuelCollected;
+	}
+}
