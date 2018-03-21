ce9846646a9f1bef7825102e33f526502a79823d
Michael Zoech <michi.zoech@gmail.com>
2018-03-20 23:38:51 -0700

    Fuel sound and rotating diamond player

---
+++ Assets/Scripts/AnimRotator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimRotator : MonoBehaviour {
+
+	private Vector3 rotation;
+
+	void Start() {
+		rotation = transform.rotation.eulerAngles;
+	}
+
+	void Update() {
+		rotation.y += 200.0f * Time.deltaTime;
+		if (rotation.y > 360.0f) {
+			rotation.y -= 360.0f;
+		}
+	}
+
+	void LateUpdate () {
+		transform.rotation = Quaternion.Euler(rotation);
+	}
+}
--- Assets/Scripts/CollectFuel.cs
+++ Assets/Scripts/CollectFuel.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class CollectFuel : MonoBehaviour {
 
 	private GameController gameController;
+	private AudioSource audioSource;
 
 	void Start() {
 		GameObject gameControllerObject = GameObject.FindWithTag("GameController");
 		gameController = gameControllerObject.GetComponent<GameController>();
+		audioSource = GetComponent<AudioSource>();
 	}
 
 	void OnTriggerEnter(Collider other) {
-		Destroy(gameObject);
 		gameController.OnFuelCollected();
+		audioSource.Play();
+		GetComponent<Renderer>().enabled = false;
+		GetComponent<Collider>().enabled = false;
+		Destroy(gameObject, audioSource.clip.length);
 	}
 }
