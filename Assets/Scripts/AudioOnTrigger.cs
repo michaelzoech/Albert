926d40207434c42a6b0118c6659462b690e96628
Michael Zoech <michi.zoech@gmail.com>
2018-03-22 18:06:57 -0700

    Audio for hitting trigger button

---
+++ Assets/Scripts/AudioOnTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioOnTrigger : MonoBehaviour {
+
+	private AudioSource audio;
+	private Trigger trigger;
+
+	private bool played;
+
+	void Start () {
+		audio = GetComponent<AudioSource>();
+		trigger = GetComponent<Trigger>();
+		played = trigger.Triggered;
+	}
+	
+	void Update () {
+		if (trigger.Triggered && !played) {
+			audio.Play();
+			played = true;
+		} else if (!trigger.Triggered && played) {
+			played = false;
+		}
+	}
+}
