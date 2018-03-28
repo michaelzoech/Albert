998a2cea177f6be9d76ac447379d26158da4ce2c
Michael Zoech <michi.zoech@gmail.com>
2018-03-27 18:47:39 -0700

    Use animations for outro too

---
+++ Assets/Scripts/ExtroController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ExtroController : MonoBehaviour {
+
+    [SerializeField]
+    private float waitTime;
+
+    [SerializeField]
+    private string nextScene;
+
+    private Coroutine WaitRoutine;
+
+    void Start() {
+        WaitRoutine = StartCoroutine(WaitForEnd());
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("Jump") || Input.GetButtonDown("Fire1") || Input.GetMouseButtonDown(0)) {
+            StopCoroutine(WaitRoutine);
+            NextScene();
+        }
+    }
+
+    IEnumerator WaitForEnd() {
+        yield return new WaitForSeconds(waitTime);
+        NextScene();
+    }
+
+    private void NextScene() {
+        if (nextScene != "") {
+            SceneManager.LoadScene(nextScene);
+        } else {
+            Application.Quit();
+        }
+    }
+}
