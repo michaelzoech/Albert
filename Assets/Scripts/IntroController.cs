5b27b87f0f6aa15fcbc36430f6d419546b9775a0
Michael Zoech <michi.zoech@gmail.com>
2018-03-27 17:55:44 -0700

    Use Timeline and AnimationClip to animate the intro

--- Assets/Scripts/IntroController.cs
+++ Assets/Scripts/IntroController.cs
@@ -6,44 +6,25 @@ using UnityEngine.UI;
 
 public class IntroController : MonoBehaviour {
 
-    [SerializeField]
-    private Text albertText;
-    [SerializeField]
-    private Text goHomeText;
-
-    private float startTime;
+    private Coroutine WaitRoutine;
 
     void Start() {
-        albertText.gameObject.SetActive(false);
-        goHomeText.gameObject.SetActive(false);
-        startTime = Time.time;
+        WaitRoutine = StartCoroutine(WaitForIntroToEnd());
     }
 
     void Update() {
-        float time = Time.time - startTime;
-
-        if (time >= 2.0f && time < 3.0f) {
-            albertText.gameObject.SetActive(true);
-            albertText.color = new Color(1.0f, 1.0f, 1.0f, 1.0f * (time - 2.0f));
-        } else if (time >= 3.0f && time < 5.0f) {
-            albertText.gameObject.SetActive(true);
-            albertText.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-        } else if (time >= 5.0f && time < 6.0f) {
-            albertText.gameObject.SetActive(true);
-            albertText.color = new Color(1.0f, 1.0f, 1.0f, 1.0f * (1.0f - time + 5.0f));
-        } else if (time >= 7.0f && time < 8.0f) {
-            albertText.gameObject.SetActive(false);
-            goHomeText.gameObject.SetActive(true);
-            goHomeText.color = new Color(1.0f, 1.0f, 1.0f, 1.0f * (time - 7.0f));
-        } else if (time >= 9.0f && time < 10.0f) {
-            goHomeText.gameObject.SetActive(true);
-            goHomeText.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-        } else if (time >= 10.0f && time < 11.0f) {
-            goHomeText.gameObject.SetActive(true);
-            goHomeText.color = new Color(1.0f, 1.0f, 1.0f, 1.0f * (1.0f - time + 10.0f));
-        } else if (time >= 11.5f) {
-            goHomeText.gameObject.SetActive(false);
-            SceneManager.LoadScene("Level01");
+        if (Input.GetButtonDown("Jump") || Input.GetButtonDown("Fire1") || Input.GetMouseButtonDown(0)) {
+            StopCoroutine(WaitRoutine);
+            LoadLevel();
         }
+    }
+
+    IEnumerator WaitForIntroToEnd() {
+        yield return new WaitForSeconds(11.0f);
+        LoadLevel();
+    }
+
+    private void LoadLevel() {
+        SceneManager.LoadScene("Level01");
     }
 }
